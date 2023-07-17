4e86cd92c6e4027d219d8aa34a95722629f03f1a
Log45 <Skyker604@gmail.com>
2023-07-17 10:58:30 -0400

    template and function for generating

--- demo.py
+++ demo.py
@@ -11,4 +11,50 @@ Goals: Using few-shot CoT, answer these questions with different prompts
     - (maybe) How long was the target irradiated?
     - (maybe) How long did the separation process take?
 """
+from pdf_parser import pdf_to_context
+import galai as gal
 
+example_context = "After irradiation of 5 h, the 64Ni target was dissolved in 6 M hydrochloride acid, and then the solution was load to an anion exchange column to separate into different components. The 64Ni was washed out with 6 M HCl and collected for recycling. Due to the elevated cost of enriched 64Ni, recycling of the target material for re-use could reduce the production cost of 64Cu, without sacriﬁcing the quality of subsequent 64Cu production. When the eluted was switched to 1 M HCl, the ﬁrst band coming out was co-produced cobalt radioisotopes (approximately 1 mL), and the second was the 64Cu, which was collected and evaporated to dryness. The residue was dissolved in 0.1 M HCl for further use. The separation process of 64Cu took about 2.5 h after irradiation."
+
+target_question = "What is the target material in the above reaction?"
+target_example_answer = "Since they say: 'the 64Ni target was dissolved in 6 M hydrochloride acid', then 64Ni, or Nickel-64, must be the target."
+
+acid_question = "What acid is the target material dissolved in during the above reaction?"
+acid_example_answer = "Since they say: 'the 64Ni target was dissolved in 6 M hydrochloride acid', then 6M hydrochloride acid must be the acid used to dissolve the target."
+
+resin_question = "What resin/column is the solution loaded into during the above reaction?"
+resin_example_answer = "Since they say: 'the solution was load to an anion exchange column to separate into different components', and an 'anion exchange column' is a resin/column, then an anion exchange column must be the resin/column."
+
+elution_question = "What acid is used in the elution during the above reaction?"
+elution_example_answer = "Since they say: 'When the eluted was switched to 1 M HCl', then 1 M HCl must be the acid used in the elution."
+
+products_question = "What are the products of the above reaction?"
+products_example_answer = "Since they say: 'the ﬁrst band coming out was co-produced cobalt radioisotopes (approximately 1 mL), and the second was the 64Cu, which was collected', and the first and second bands represent products, then cobalt radioisotopes and 64Cu, or Copper-64, must be the products."
+
+target_example = f"Context: {example_context}\n Question: {target_question}\n Answer: {target_example_answer}"
+acid_example = f"Context: {example_context}\n Question: {acid_question}\n Answer: {acid_example_answer}"
+resin_example = f"Context: {example_context}\n Question: {resin_question}\n Answer: {resin_example_answer}"
+elution_example = f"Context: {example_context}\n Question: {elution_question}\n Answer: {elution_example_answer}"
+products_example = f"Context: {example_context}\n Question: {products_question}\n Answer: {products_example_answer}"
+
+questions = [target_question, acid_question, resin_question, elution_question, products_question]
+questions_examples_dict = {target_question : target_example,
+                           acid_question : acid_example,
+                           resin_question : resin_example,
+                           elution_question : elution_example,
+                           products_question : products_example}
+
+def main():
+    """"""
+    contexts = pdf_to_context()
+    model = gal.load_model("standard")
+    for context in contexts:
+        for q in questions:
+            example = questions_examples_dict[q]
+            input = f"{example}\n Context: {context}\n Question: {q}\n Answer: "
+            model.generate(input, max_length=400)
+    
+
+
+if __name__ == "__main__":
+    main()
